d5b5f363e85b10125b157c3fa8b9d59669f553a6
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-10-08 12:54:39 +0300

    Add project files.

---
+++ Models/Siniflar/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Siniflar
+{
+    public class Kategori
+    {
+        [Key]
+        public int KategıriId { get; set; }
+        public string KategoriAd { get; set; }
+    }
+}
